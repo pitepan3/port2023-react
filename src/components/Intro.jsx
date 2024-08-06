208279865bd83c7e503b19b1fc2c596ee66d24b8
donghoonchoi <pitepan3@naver.com>
2024-08-06 20:33:43 +0900

    인트로 영역 완성

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,44 +2,48 @@ import React from "react";
 
 import about from "../assets/img/about.jpg";
 
+const introText = {
+  title: "port developer",
+  desc: ["talent is", "found at the end of the", "effort"]
+}
+
 const Intro = () => {
   return (
-    // <section id="intro">
-    //   <div class="intro__inner">
-    //     <h2 class="intro__title">
-    //       port developer
-    //     </h2>
-    //     <div class="intro__lines" aria-hidden="true">
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //     </div>
-    //     <div class="intro__text">
-    //       <div class="text">
-    //         <div>talent is</div>
-    //         <div>found at the end of the</div>
-    //         <div>effort</div>
-    //       </div>
-    //       <div class="img">
-    //         <img src={about} alt="어바웃미" />
-    //       </div>
-    //     </div>
-    //     <div class="intro__lines bottom" aria-hidden="true">
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //       <span class="line"></span>
-    //     </div>
-    //   </div>
-    // </section>
-    <div>Intro</div>
+    <section id="intro">
+      <div className="intro__inner">
+        <h2 className="intro__title">
+          {introText.title}
+        </h2>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro__text">
+          <div className="text">
+            <div>{introText.desc[0]}</div>
+            <div>{introText.desc[1]}</div>
+            <div>{introText.desc[2]}</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="어바웃미" />
+          </div>
+        </div>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
