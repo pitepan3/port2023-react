a636c5ab09ad1c7ed17707a8858a6dee01f1f6d5
donghoonchoi <pitepan3@naver.com>
2024-08-06 19:57:40 +0900

    헤더영역 완성

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import about from "../assets/img/about.jpg";
 
 const Intro = () => {
   return (
+    // <section id="intro">
+    //   <div class="intro__inner">
+    //     <h2 class="intro__title">
+    //       port developer
+    //     </h2>
+    //     <div class="intro__lines" aria-hidden="true">
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //     </div>
+    //     <div class="intro__text">
+    //       <div class="text">
+    //         <div>talent is</div>
+    //         <div>found at the end of the</div>
+    //         <div>effort</div>
+    //       </div>
+    //       <div class="img">
+    //         <img src={about} alt="어바웃미" />
+    //       </div>
+    //     </div>
+    //     <div class="intro__lines bottom" aria-hidden="true">
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //       <span class="line"></span>
+    //     </div>
+    //   </div>
+    // </section>
     <div>Intro</div>
   )
 }
